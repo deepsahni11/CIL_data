c21b96180b461cdfef5b70f16f08712f10a3eb44
deepsahni11 <na16b110@smail.iitm.ac.in>
2020-03-20 10:15:37 +0530

    Update data_creation_13_features.py

--- data_creation_13_features.py
+++ data_creation_13_features.py
@@ -154,7 +154,7 @@ for w in weights:
 #         break
                         
 
-np.save('../Data_metrics_13d_datasets_X_train.npy',X_train_datasets_13d_resampled)
-np.save('../Data_metrics_13d_datasets_X_test.npy',X_test_datasets_13d_resampled)
-np.save('../Data_metrics_13d_datasets_y_train.npy',y_train_datasets_13d_resampled)
-np.save('../Data_metrics_13d_datasets_y_test.npy',y_test_datasets_13d_resampled)
+np.save('../datasets_13d_X_train_resampled.npy',X_train_datasets_13d_resampled)
+np.save('../datasets_13d_X_test_resampled.npy',X_test_datasets_13d_resampled)
+np.save('../datasets_13d_y_train_resampled.npy',y_train_datasets_13d_resampled)
+np.save('../datasets_13d_y_test_resampled.npy',y_test_datasets_13d_resampled)
